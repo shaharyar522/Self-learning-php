f1f17a2d0932f4da50927401c0dc1b96cf5289f0
shaharyar522 <shaharyar.khan4511@gmail.com>
2025-03-05 22:13:43 +0500

    Today i am learning php dates in different way how to use to and implement in dates with month time and years different way

---
+++ 17_dates.php
@@ -0,0 +1,111 @@
+<?php
+// hum es main date ko dahkian guy jo ky php main use hnti hian
+
+// m for mont
+// d for date
+// y for years
+
+echo "Hello i am shari now i will describe you date on php <br>";
+
+$d = date("l jS \of F Y h:i:s A");
+echo "Today date is  $d <br>";
+
+echo " anothe way date show <br>";
+
+
+/// uay wali date hamray pass date behi hain or month behi hain or 
+// or years behi hina
+$d = date("F j, Y, g:i a");
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+echo " anothe way date show <br>";
+
+$d = date("d .m. y");
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+echo "anothe way date show <br>";
+
+$d = date("Ymd"); 
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+
+
+echo " anothe way date show <br>";
+
+$d =date('h-i-s, j-m-y, it is w Day');
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+
+echo " anothe way date show <br>";
+
+$d =date('\i\t \i\s \t\h\e jS \d\a\y.');
+echo "Today date is  $d <br>";
+
+
+echo "<br>";
+echo "<br>";
+
+
+echo " anothe way date show <br>";
+
+$d =date("D M j G:i:s T Y");  
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+echo " anothe way date show <br>";
+
+$d =date("D M j G:i:s T Y");   
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+
+echo " anothe way date show <br>";
+
+$d =date("H:i:s");      
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+
+echo " anothe way date show <br>";
+
+$d =date("H:i:s");      
+echo "Today date is  $d <br>";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Thank you php for understanting for php date different way /// shari";
+
+
+
+
+
+
+
+
+?>
