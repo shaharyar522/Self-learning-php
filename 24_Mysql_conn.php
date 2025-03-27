224657e339ab71bc0f603e41be1c255505d11724
shaharyar522 <shaharyar.khan4511@gmail.com>
2025-03-27 22:14:40 +0500

    understadning of Mysql connetion and how to connected database

--- 24_Mysql_conn.php
+++ 24_Mysql_conn.php
@@ -2,12 +2,28 @@
 
 // uay hamray pass connection hain mysql main jo kay hum use karay guy 
 
-echo "Well come to  the  stage  where we are  ready  to get  connected  to database ";
+echo "Well come to  the  stage  where we are  ready  to get  connected  to database <br> ";
 
 
 // ways to connected to database 
 // 1.mysqli  extention 
 // 2. PDO
+
+//connected to a database 
+
+$servername = "localhost";
+$username  = "root";
+$password = "";
+$databasename = "";
+
+
+
+
+$conn  = mysqli_connect($servername, $username, $password,$databasename);
+
+if(!$conn){
+   die("Sorry Your database is not connected : ".mysqli_connect_error());
+}
 
 
 
